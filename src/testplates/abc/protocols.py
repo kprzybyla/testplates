3651d2c170a85de57f5b65224bea84c93acf3cbf
Krzysztof Przybyła <krzysztof.h.przybyla@gmail.com>
2020-02-01 00:09:21 +0100

    Added docstring for abc.protocols

--- src/testplates/abc/protocols.py
+++ src/testplates/abc/protocols.py
@@ -6,72 +6,56 @@ from typing_extensions import runtime_checkable, Protocol
 
 @runtime_checkable
 class SupportsExclusiveBoundaries(Protocol):
+
+    """
+        Protocol for object with exclusive boundaries.
+    """
+
     def __gt__(self, other: Any) -> bool:
 
         """
-            ...
+            Rich comparison for "greater than".
 
-            :param other:
+            :param other: any other object
         """
 
     def __lt__(self, other: Any) -> bool:
 
         """
-            ...
+            Rich comparison for "less than".
 
-            :param other:
+            :param other: any other object
         """
 
 
 @runtime_checkable
 class SupportsInclusiveBoundaries(Protocol):
+
+    """
+        Protocol for object with inclusive boundaries.
+    """
+
     def __ge__(self, other: Any) -> bool:
 
         """
-            ...
+            Rich comparison for "greater or equal to".
 
-            :param other:
+            :param other: any other object
         """
 
     def __le__(self, other: Any) -> bool:
 
         """
-            ...
+            Rich comparison for "less or equal to".
 
-            :param other:
+            :param other: any other object
         """
 
 
 @runtime_checkable
 class SupportsBoundaries(SupportsExclusiveBoundaries, SupportsInclusiveBoundaries, Protocol):
-    def __gt__(self, other: Any) -> bool:
 
-        """
-            ...
-
-            :param other:
-        """
-
-    def __lt__(self, other: Any) -> bool:
-
-        """
-            ...
-
-            :param other:
-        """
-
-    def __ge__(self, other: Any) -> bool:
-
-        """
-            ...
-
-            :param other:
-        """
-
-    def __le__(self, other: Any) -> bool:
-
-        """
-            ...
-
-            :param other:
-        """
+    """
+        Protocol for object with both
+        exclusive and inclusive boundaries.
+    """
