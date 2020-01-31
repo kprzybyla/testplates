be4b6b462a434dd1c02335c2b2f43be92c61fed4
Krzysztof Przybyła <krzysztof.h.przybyla@gmail.com>
2020-01-31 23:28:54 +0100

    Added docstring and improvements to utils.compare func

--- src/testplates/utils.py
+++ src/testplates/utils.py
@@ -8,13 +8,25 @@ T = TypeVar("T")
 
 
 def compare(self_value: Maybe[Value[T]], other_value: Maybe[Value[T]]) -> bool:
+
+    """
+        Compares self value and other value and
+        returns True if they match, otherwise False.
+
+        Assumes that special values were validated
+        against field types and do not bend any logic.
+
+        :param self_value: self template value
+        :param other_value: other object value
+    """
+
     if self_value is WILDCARD:
         return True
 
-    if other_value is not Missing and self_value is ANY:
+    if self_value is ANY and other_value is not Missing:
         return True
 
-    if other_value is Missing and self_value is ABSENT:
+    if self_value is ABSENT and other_value is Missing:
         return True
 
     return self_value == other_value
