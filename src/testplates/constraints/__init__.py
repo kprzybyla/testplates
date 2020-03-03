b80c8c62b04adcfbc7c17c67a634810c5861ea6b
Krzysztof Przybyła <krzysztof.h.przybyla@gmail.com>
2020-03-03 21:33:02 +0100

    File names changes for constraints

--- src/testplates/constraints/__init__.py
+++ src/testplates/constraints/__init__.py
@@ -7,9 +7,9 @@ __all__ = [
     "is_permutation_of",
 ]
 
-from .container import contains
-from .sized import has_length
-from .range import ranges_between
-from .pattern import matches_pattern
-from .one_of import is_one_of
-from .permutation import is_permutation_of
+from .contains_constraint import contains
+from .has_length_constraint import has_length
+from .ranges_between_constraint import ranges_between
+from .matches_pattern_constraint import matches_pattern
+from .is_one_of_constraint import is_one_of
+from .is_permutation_of_constraint import is_permutation_of
