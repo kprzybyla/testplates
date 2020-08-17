f7ec91bd8f109a6743ed15011a70d75f8f80984b
Krzysztof Przybyła <krzysztof.h.przybyla@gmail.com>
2020-08-17 19:08:17 +0200

    Use pattern name instead of regex in matches_pattern tests

--- tests/test_constraints/test_matches_pattern.py
+++ tests/test_constraints/test_matches_pattern.py
@@ -22,22 +22,22 @@ BYTES_PATTERNS: Final[List[bytes]] = list(map(str.encode, STR_PATTERNS))
 
 PATTERNS: Final[List[Union[str, bytes]]] = [*STR_PATTERNS, *BYTES_PATTERNS]
 
-# TODO(kprzybyla): Implement generic st_regex after following issue is resolved:
+# TODO(kprzybyla): Implement generic st_from_pattern after following issue is resolved:
 #                  https://github.com/HypothesisWorks/hypothesis/issues/2365
 
 
 @st.composite
-def st_str_regex(draw: Draw[str], pattern: str) -> str:
+def st_from_str_pattern(draw: Draw[str], pattern: str) -> str:
     return draw(st.from_regex(pattern, fullmatch=True))
 
 
 @st.composite
-def st_bytes_regex(draw: Draw[bytes], pattern: bytes) -> bytes:
+def st_from_bytes_pattern(draw: Draw[bytes], pattern: bytes) -> bytes:
     return draw(st.from_regex(pattern, fullmatch=True))
 
 
 @st.composite
-def st_inverse_str_regex(draw: Draw[str], pattern: str) -> str:
+def st_from_str_pattern_inverse(draw: Draw[str], pattern: str) -> str:
     text = draw(st.text())
     assume(not re.match(pattern, text))
 
@@ -45,7 +45,7 @@ def st_inverse_str_regex(draw: Draw[str], pattern: str) -> str:
 
 
 @st.composite
-def st_inverse_bytes_regex(draw: Draw[bytes], pattern: bytes) -> bytes:
+def st_from_bytes_pattern_inverse(draw: Draw[bytes], pattern: bytes) -> bytes:
     binary = draw(st.binary())
     assume(not re.match(pattern, binary))
 
@@ -65,7 +65,7 @@ def test_repr(pattern: AnyStr) -> None:
 @given(data=st.data())
 @pytest.mark.parametrize("pattern", STR_PATTERNS)
 def test_returns_true_with_str_pattern(data: st.DataObject, pattern: str) -> None:
-    value = data.draw(st_str_regex(pattern))
+    value = data.draw(st_from_str_pattern(pattern))
 
     constraint = matches_pattern(pattern)
 
@@ -76,7 +76,7 @@ def test_returns_true_with_str_pattern(data: st.DataObject, pattern: str) -> Non
 @given(data=st.data())
 @pytest.mark.parametrize("pattern", BYTES_PATTERNS)
 def test_returns_true_with_bytes_pattern(data: st.DataObject, pattern: bytes) -> None:
-    value = data.draw(st_bytes_regex(pattern))
+    value = data.draw(st_from_bytes_pattern(pattern))
 
     constraint = matches_pattern(pattern)
 
@@ -87,7 +87,7 @@ def test_returns_true_with_bytes_pattern(data: st.DataObject, pattern: bytes) ->
 @given(data=st.data())
 @pytest.mark.parametrize("pattern", STR_PATTERNS)
 def test_returns_false_with_str_pattern(data: st.DataObject, pattern: str) -> None:
-    value = data.draw(st_inverse_str_regex(pattern))
+    value = data.draw(st_from_str_pattern_inverse(pattern))
 
     constraint = matches_pattern(pattern)
 
@@ -98,7 +98,7 @@ def test_returns_false_with_str_pattern(data: st.DataObject, pattern: str) -> No
 @given(data=st.data())
 @pytest.mark.parametrize("pattern", BYTES_PATTERNS)
 def test_returns_false_with_bytes_pattern(data: st.DataObject, pattern: bytes) -> None:
-    value = data.draw(st_inverse_bytes_regex(pattern))
+    value = data.draw(st_from_bytes_pattern_inverse(pattern))
 
     constraint = matches_pattern(pattern)
 
@@ -109,7 +109,7 @@ def test_returns_false_with_bytes_pattern(data: st.DataObject, pattern: bytes) -
 @given(data=st.data())
 @pytest.mark.parametrize("pattern", STR_PATTERNS)
 def test_returns_false_with_str_pattern_and_bytes_value(data: st.DataObject, pattern: str) -> None:
-    value = data.draw(st_str_regex(pattern))
+    value = data.draw(st_from_str_pattern(pattern))
 
     constraint = matches_pattern(pattern)
 
@@ -122,7 +122,7 @@ def test_returns_false_with_str_pattern_and_bytes_value(data: st.DataObject, pat
 def test_returns_false_with_bytes_pattern_and_str_value(
     data: st.DataObject, pattern: bytes
 ) -> None:
-    value = data.draw(st_bytes_regex(pattern))
+    value = data.draw(st_from_bytes_pattern(pattern))
 
     constraint = matches_pattern(pattern)
 
