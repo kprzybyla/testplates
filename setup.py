3a42014f10f1fa6c6ab428287887811e8739c08a
Krzysztof Przybyła <krzysztof.h.przybyla@gmail.com>
2020-09-02 21:47:42 +0200

    Update black from master to 20.8b1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 RESULTFUL: Final[str] = "resultful == 1.0.0a0"
 
-BLACK: Final[str] = "black @ git+https://github.com/psf/black.git@master"
+BLACK: Final[str] = "black == 20.8b1"
 FLAKE8: Final[str] = "flake8 ~= 3.8.0"
 MYPY: Final[str] = "mypy @ git+https://github.com/kprzybyla/mypy.git@testplates"
 
